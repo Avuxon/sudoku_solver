9aad43f1e657a127efa6dfc1448f603c13dad656
Sathwik Yanamaddi <sathwik45@gmail.com>
2021-12-20 17:12:07 -0500

    Enter

---
+++ sudoku_solver.py
@@ -0,0 +1,19 @@
+# This is the main file which holds functions to modify the board and
+# build the backtracking algo
+
+def create_board(): 
+	board = [
+            [0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0]
+            ]
+	return board
+
+def print_board(board): 
+
